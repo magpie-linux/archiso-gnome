8f7a1d9f0f375b2dab53586debf7d022bcd1b48a
Rizwan Hasan <rizwan.hasan486@gmail.com>
2019-09-03 00:31:35 +0600

    Saving files before refreshing line endings

--- build.py
+++ build.py
@@ -9,51 +9,26 @@ import sys
 class MainWork:
 
     def __init__(self):
-        self.lsb_release = None
-        self.magpie_release = None
-        self.os_release = None
         self.verInfo = None
-
-        with open('version.json', 'r') as file:
+        self.scriptData = None
+        with open('version.json', 'r', encoding='utf-8') as file:
             self.verInfo = json.loads(file.read())
-
-        with open('build.sample.sh', 'r') as file:
+        with open('build.sample.sh', 'r', encoding='utf-8') as file:
             self.scriptData = file.read()
-
         self.__clean()
-        self.__copyPackages()
+        self.__copyProcess()
         self.__distroInfoProcess()
         self.__scriptBuilding()
         self.__runCommand()
         self.__clean()
 
+    # noinspection PyMethodMayBeStatic
     def __distroInfoProcess(self):
-        self.lsb_release = (
-                """LSB_VERSION=1.4
-        DISTRIB_ID=""" + self.verInfo['distro name'] + """
-        DISTRIB_RELEASE=""" + self.verInfo['version'] + """
-        DISTRIB_CODENAME=""" + self.verInfo['codename'] + """
-        DISTRIB_DESCRIPTION=""" + "\"" + self.verInfo['distro name'] + "\""
-        )
-        self.magpie_release = self.verInfo['distro name'] + " " + self.verInfo['codename']
-        self.os_release = (
-                """NAME=""" + self.verInfo['distro name'] + """
-        PRETTY_NAME=""" + self.verInfo['pretty name'] + """
-        ID=""" + self.verInfo['distro name'] + """
-        ID_LIKE=""" + self.verInfo['base'] + """
-        VERSION_ID=""" + self.verInfo['version'] + """
-        ANSI_COLOR="0;36"
-        """
-        )
-
-        with open('airootfs/etc/skel/.magpie-settings/' + 'lsb-release', 'w') as file:
-            file.write(self.lsb_release)
-
-        with open('airootfs/etc/skel/.magpie-settings/' + 'os-release', 'w') as file:
-            file.write(self.os_release)
-
-        with open('airootfs/etc/skel/.magpie-settings/' + 'magpie-release', 'w') as file:
-            file.write(self.magpie_release)
+        releaseFiles: str = 'airootfs/etc/skel/settings/*-release'
+        subprocess.call("sed -i 's|__RELEASE__|{0}|' {1}"
+                        .format(self.verInfo['version'], releaseFiles), shell=True)
+        subprocess.call("sed -i 's|__CODENAME__|{0}|' {1}"
+                        .format(self.verInfo['codename'], releaseFiles), shell=True)
 
     def __scriptBuilding(self):
         self.scriptData = self.scriptData.replace("#SHELL_LOCATION", "#!/bin/bash")
@@ -68,36 +43,40 @@ class MainWork:
         self.scriptData = self.scriptData.replace(
             'iso_application="@"', 'iso_application=' + "\"" + self.verInfo['iso application'] + "\"")
 
-        with open('build.sh', 'w') as file:
+        with open('build.sh', 'w', encoding='utf-8') as file:
             file.write(self.scriptData)
         subprocess.call('chmod +x build.sh', shell=True)
+
+    # noinspection PyMethodMayBeStatic
+    def __copyProcess(self):
         subprocess.call('mkdir -v airootfs/root', shell=True)
         # subprocess.call('chmod 777 airootfs/root', shell=True)
         subprocess.call('cp -vf root_customizer.sh airootfs/root/customize_airootfs.sh', shell=True)
-        subprocess.call('cp -vf .automated_script.sh airootfs/root/.automated_script.sh', shell=True)
+        subprocess.call('cp -vf automated_script.sh airootfs/root/.automated_script.sh', shell=True)
         subprocess.call('chmod +x airootfs/root/customize_airootfs.sh', shell=True)
         subprocess.call('chmod 777 airootfs/root/customize_airootfs.sh', shell=True)
-
-    def __copyPackages(self):
         subprocess.call('cp -avr packages airootfs/etc/skel/', shell=True)
+        subprocess.call('cp -avr settings airootfs/etc/skel/', shell=True)
 
+    # noinspection PyMethodMayBeStatic
     def __runCommand(self):
         check: int = os.system('./build.sh -v')
         if check is 0:
             os.system('chmod 777 ISO_Image ISO_Image/*')
 
+    # noinspection PyMethodMayBeStatic
     def __clean(self):
         clean()
 
 
 def clean():
+    print('Cleaning..')
     subprocess.call('rm -rf airootfs/root', shell=True)
-    subprocess.call('rm -rf build.sh build_work', shell=True)
+    subprocess.call('rm -rf airootfs/etc/skel/settings', shell=True)
     subprocess.call('rm -rf airootfs/etc/skel/packages', shell=True)
-    subprocess.call('rm -rf airootfs/etc/skel/.magpie-settings/os-release', shell=True)
-    subprocess.call('rm -rf airootfs/etc/skel/.magpie-settings/lsb-release', shell=True)
-    subprocess.call('rm -rf airootfs/etc/skel/.magpie-settings/magpie-release', shell=True)
-    print('Cleaned..')
+    subprocess.call('rm -rf build.sh', shell=True)
+    subprocess.call('rm -rf build_work', shell=True)
+    print('Cleaned')
 
 
 def main():
--- root_customizer.sh
+++ root_customizer.sh
@@ -10,10 +10,12 @@ cp -aT /etc/skel/ /root/
 chmod 700 /root
 # ##############################################
 
+
 # ### Fixing Permisssion ##
 chown root:root /
 chmod 755 /
 # #########################
+
 
 # ################################################## Creating liveuser #####################################################
 useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash liveuser
@@ -22,12 +24,14 @@ chown -R liveuser:users /home/liveuser
 chmod 700 /home/liveuser/
 # ##########################################################################################################################
 
+
 # ### Giving root permission for live user /etc/sudoers ####
 rm -v /etc/sudoers
-mv -v /etc/skel/.magpie-settings/sudoers-backup /etc/sudoers
+mv -v /etc/skel/settings/sudoers-backup /etc/sudoers
 chown -c root:root /etc/sudoers
 chmod -c 0440 /etc/sudoers
 # ##########################################################
+
 
 # ############### Importing pacman keys ############
 pacman-key --init
@@ -35,22 +39,26 @@ pacman-key --populate archlinux
 pacman-key --refresh-keys
 # ##################################################
 
+
 # ##################### OS Information ########################
 rm -v /etc/lsb-release
-mv -v /etc/skel/.magpie-settings/lsb-release /etc/lsb-release
+mv -v /etc/skel/settings/lsb-release /etc/lsb-release
 rm -v /usr/lib/os-release
-mv -v /etc/skel/.magpie-settings/os-release /usr/lib/os-release
+mv -v /etc/skel/settings/os-release /usr/lib/os-release
 # #############################################################
 
-# ###################### GDM Wayland Disabling ##################
-rm -v /etc/gdm/custom.conf
-mv -v /etc/skel/.magpie-settings/custom.conf /etc/gdm/custom.conf
-# ###############################################################
 
-# ########## Adding grub-theme to /etc/default/grub file ##########
+# ############## GDM Wayland Disabling ##################
+rm -v /etc/gdm/custom.conf
+mv -v /etc/skel/settings/custom.conf /etc/gdm/custom.conf
+# #######################################################
+
+
+# ###### Adding grub-theme to /etc/default/grub file ######
 rm -v /etc/default/grub
-mv -v /etc/skel/.magpie-settings/etc-default-grub /etc/default/grub
-# #################################################################
+mv -v /etc/skel/settings/etc-default-grub /etc/default/grub
+# #########################################################
+
 
 # #############################################################################
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
@@ -61,24 +69,28 @@ sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 # #############################################################################
 
+
 # # ############################# Removing packages ##########################################################
 pacman -R --noconfirm swell-foop tali gnome-mines gnome-tetravex gnome-recipes accerciser gnome-boxes
 pacman -R --noconfirm gnome-nibbles gnome-sudoku hitori quadrapassel gnome-builder devhelp lftp gnome-software
 pacman -R --noconfirm gnome-robots five-or-more four-in-a-row gnome-mahjongg ipython gnome-backgrounds
-pacman -R --noconfirm gnome-klotski gnome-taquin iagno lightsoff polari gnome-multi-writer epiphany 
+pacman -R --noconfirm gnome-klotski gnome-taquin iagno lightsoff polari gnome-multi-writer epiphany
 # # ##########################################################################################################
 
+
 # ######### Installing custom packages to rootfs ##########
-cp -vf /etc/skel/.magpie-settings/mirrorlist /etc/pacman.d/
+cp -vf /etc/skel/settings/mirrorlist /etc/pacman.d/
 pacman -Syyu --noconfirm
 cd /etc/skel/packages
 pacman -U --noconfirm *.pkg.tar.xz
 # #########################################################
 
+
 # ### Changing pacman.conf for magpie-mirrrorlist support ##
 rm -drv /etc/pacman.conf
-cp -v /etc/skel/.magpie-settings/pacman.conf /etc/
+cp -v /etc/skel/settings/pacman.conf /etc/
 # ##########################################################
+
 
 # # ############################ MagpieOS Install Desktop File #####################################
 # cp -v /usr/share/applications/calamares.desktop /home/liveuser/.config/autostart/calamares.desktop
@@ -86,38 +98,46 @@ cp -v /etc/skel/.magpie-settings/pacman.conf /etc/
 # chmod +x /home/liveuser/.config/autostart/calamares.desktop
 # # ################################################################################################
 
+
 # ########## Adding custom /etc/nanorc for Nano ########
-mv -vf /etc/skel/.magpie-settings/etc-nanorc /etc/nanorc
+mv -vf /etc/skel/settings/etc-nanorc /etc/nanorc
 # ######################################################
+
 
 # ######## Copying release info of MagpieOS to livecd ########
 rm /etc/arch-release
-cp /etc/skel/.magpie-settings/magpie-release /etc/
-cp /etc/skel/.magpie-settings/magpie-release /etc/arch-release
+cp /etc/skel/settings/magpie-release /etc/
+cp /etc/skel/settings/magpie-release /etc/arch-release
 # ############################################################
 
-# ######### Adding custom mkinitcpio config ###########
-mv -vf /etc/skel/.magpie-settings/mkinitcpio.conf /etc/
-# #####################################################
 
-# ######## Adding custom ntp config ########
+# ###### Adding custom mkinitcpio config ######
+mv -vf /etc/skel/settings/mkinitcpio.conf /etc/
+# #############################################
+
+
+# #### Adding custom ntp config ####
 rm /etc/ntp.conf
-cp /etc/skel/.magpie-settings/ntp.conf /etc/
-# ##########################################
+cp /etc/skel/settings/ntp.conf /etc/
+# ##################################
 
-# ######## Adding red lined bash theme for root ##########
-mv -f /etc/skel/.magpie-settings/bashrc_root /root/.bashrc
-# ########################################################
+
+# ##### Adding red lined bash theme for root #####
+mv -f /etc/skel/settings/bashrc_root /root/.bashrc
+# ################################################
+
 
 # ###################### Adding MagpieOS Logo in gdm login screen #############################
 sudo -u gdm dbus-launch gsettings set org.gnome.login-screen logo '/etc/skel/.mapieos-logo.png'
 dconf update
 # #############################################################################################
 
+
 # ###################### Adding cursor theme in  gdm login screen ########################
 sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme 'Neon-Blue'
 dconf update
 # ########################################################################################
+
 
 # ####################### Tap to click support for gnome settings ############################
 rm -rf /usr/share/X11/xorg.conf.d/70-synaptics.conf
@@ -125,18 +145,22 @@ sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.touchpad tap
 dconf update
 # ############################################################################################
 
+
 # ########### Adding Gnome File Manager(Nautilus) drive mount without password #############
-cp -f /etc/skel/.magpie-settings/org.freedesktop.UDisks2.policy /usr/share/polkit-1/actions/
+cp -f /etc/skel/settings/org.freedesktop.UDisks2.policy /usr/share/polkit-1/actions/
 # ##########################################################################################
 
+
 # ###############################
-rm -dr /etc/skel/.magpie-settings
-rm -dr /etc/skel/packages
+rm -rfv /etc/skel/settings
+rm -rfv /etc/skel/packages
 # ###############################
+
 
 # ## Unmuting speakers ##
 amixer sset Master unmute
 # #######################
+
 
 # ###########################################################################
 systemctl enable pacman-init.service choose-mirror.service NetworkManager gdm
